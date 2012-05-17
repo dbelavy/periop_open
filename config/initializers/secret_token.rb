81af1694204c89f3ef06917f6986a571018f41ad
Alexander Khitev <alexander.khitev@gmail.com>
2012-05-17 19:32:35 +0400

    rails project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Periop::Application.config.secret_token = '293f83930d45f95fdb223c6e81f971f1ccb5f3d1df3083dcdfedc214f3bdeeb3e99eff3f4bb3de0a455fa2a0d22cc4f2d12eb5fe98b3f92c9770abb0dc2111f8'
