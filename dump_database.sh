38d55f9b0bbeca3bd35f3ff38c27142155dbd339
Alexander Khitev <Alexander.khitev@gmail.com>
2012-09-09 20:22:48 +0300

    issse #79 backup script updated

--- dump_database.sh
+++ dump_database.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mongodump --host staff.mongohq.com:10091 --db $MONGOHQ_DB -u $MONGOHQ_USER -p $MONGOHQ_PASSWORD -o ./dump/heroku/
-tar czvf dump/db_dump_$(date '+%Y%m%d').tgz dump/heroku/
+tar czvf dump/db_dump_$(date '+%Y%m%d-%H-%M').tgz dump/heroku/
 
