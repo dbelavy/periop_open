2d68f4c7b3308464f8763b2ea5f0fb12bd5d6982
David Belavy <david@belavy.com>
2012-10-29 08:33:31 +1000

    Added push to demo section for heroku scripts

--- David_commands_for_heroku.sh
+++ David_commands_for_heroku.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PS3='Please enter your choice: '
-options=("Change to Demo user" "Change to Production user" "Update questions Demo" "Update questions Production" "Ad hoc backup Demo" "Ad hoc backup Production" "Keep Demo alive" "Quit")
+options=("Change to Demo user" "Change to Production user" "Update questions Demo" "Update questions Production" "Ad hoc backup Demo" "Ad hoc backup Production" "Keep Demo alive" "Push to Demo" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -34,6 +34,19 @@ do
 				curl -kv https://pre-op-demo.herokuapp.com
 				sleep 60	
 			done
+			;;
+		"Push to Demo")
+			echo "You chose to Push to Demo"
+			echo "Set account to Demo"
+			heroku accounts:set Demo
+			echo "Backup demo"
+			heroku run rake mongo:backup --app pre-op-demo
+			echo "Push"
+			git push -v --tags Demo master:master
+			echo "Run migration script"
+			heroku run rake db:migrate --app pre-op-demo
+			echo "Update questions"
+			heroku run rake db:update_questions --app pre-op-demo
 			;;
         "Quit")
             break
