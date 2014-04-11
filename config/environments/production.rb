3217c07afcd7d49c7a528faaba0b9e49f68d10d8
David Belavy <david@belavy.com>
2014-04-12 08:11:47 +1000

    force ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Periop::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  #config.force_ssl = true
+  config.force_ssl = true
   #  config.ssl_options= {host: "www.pre-op.net"}
 
   # See everything in the log (default is :info)
